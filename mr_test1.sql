387ab77869e4b988a8ea3ddc33b62e19cfe0eeff
mohanji.rauniyar <mohanji.rauniyar@capgemini.com>
2019-02-06 12:46:56 +0000

    adding new sql file mohan

---
+++ mr_test1.sql
@@ -0,0 +1,20 @@
+create table dtag_tab4
+(
+a number 
+);
+create table dtag_tab4
+(
+a number 
+);
+create table dtag_tab4
+(
+a number 
+);
+create table dtag_tab4
+(
+a number 
+);
+create table dtag_tab4
+(
+a number 
+);
